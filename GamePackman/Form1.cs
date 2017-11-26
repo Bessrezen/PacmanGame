037b4e284dda92adab680735c917fe4b627d22af
Dmitry <mitya559@mail.ru>
2017-11-26 13:28:12 +0300

    1 commit

---
+++ GamePackman/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GamePackman
+{
+    public partial class Pacman : Form
+    {
+        public Pacman()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 records = new Form2();
+            records.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            Game game = new Game();
+            game.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Game game = new Game();
+            game.Show();
+        }
+    }
+}
