2a6e662b393f31f4cd3d82d6530a051b2e1e0a10
Dmitry <mitya559@mail.ru>
2017-11-27 16:47:53 +0300

    add SetPacman

--- GamePackman/Form1.Designer.cs
+++ GamePackman/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pacman));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -79,6 +80,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Pacman";
             this.Text = "Pacman";
             this.Load += new System.EventHandler(this.Form1_Load);
--- GamePackman/PacmanPers.cs
+++ GamePackman/PacmanPers.cs
@@ -12,6 +12,10 @@ namespace GamePackman
 {
     class PacmanPers
     {
+        int currentDirection = 0;
+        int nextDirection = 0;
+        int coordinateX = 0;
+        int coordinateY = 0;
         public PictureBox PacmanImage = new PictureBox();
         public ImageList PacmanImages = new ImageList();
         public int ImageOn = 0;
@@ -19,7 +23,19 @@ namespace GamePackman
         public PacmanPers()
         {
             PacmanImages.Images.Add(Properties.Resources.Pacman_1_0);
+
             PacmanImages.ImageSize = new Size(27, 28);
+        }
+
+        public void SetPacman()
+        {
+            // Place pacman in board
+            PacmanImage.Image = Properties.Resources.Pacman_2_1;
+            currentDirection = 0;
+            nextDirection = 0;
+            coordinateX = 0;
+            coordinateY = 0;
+
         }
     }
 }
